4f8109cd76518594af299d3162ec9516ca05d9c4
F1ashHawk <1nik2121@gmail.com>
2022-06-26 03:21:36 +0300

    Практически коплит веб

--- scripts/script.js
+++ scripts/script.js
@@ -34,30 +34,52 @@ function showSlides(n) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    // dots[slideIndex - 1].className += " active";
 }
 
-$(function() {
-    var $videoContainer = $('#video'),
-        $videoControls = $('.video-control'),
-        $video = $('#myVideo')[0];
+// function() {
+//     var $videoContainer = $('#video'),
+//         $videoControls = $('.video-control'),
+//         $video = $('#myVideo')[0];
 
-    $videoControls.click(function() {
-        if ($video.paused) {
-            $video.play();
-            $videoContainer.addClass('video-is-playing');
-        } else {
-            $video.pause();
-            $videoContainer.removeClass('video-is-playing');
-            //  возвращаем постер
-            $video.load();
-        }
-    });
-});
+//     $videoControls.click(function() {
+//         if ($video.paused) {
+//             $video.play();
+//             $videoContainer.addClass('video-is-playing');
+//         } else {
+//             $video.pause();
+//             $videoContainer.removeClass('video-is-playing');
+//             //  возвращаем постер
+//             $video.load();
+//         }
+//     });
+// };
 
-$('.multiple-items').slick({
-    infinite: true,
-    dots: true,
-    slidesToShow: 3,
-    slidesToScroll: 1
-});
+// $('.multiple-items').slick({
+//     infinite: true,
+//     dots: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1
+// });
+
+document.getElementById('start-form-open').addEventListener('click',function(){
+    document.getElementById('start-form-overlay').classList.remove('start-form-overlay-close')
+})
+
+document.getElementById('close-start-form-button').addEventListener('click',function(){
+    document.getElementById('start-form-overlay').classList.add('start-form-overlay-close')
+})
+
+// document.getElementById('start-form-overlay').addEventListener('click',function(){
+//     document.getElementById('start-form-overlay').classList.add('start-form-overlay-close')
+// })
+
+$('input[name="calc-hour-day"],input[name="calc-day-week"],input[name="calc-earn-for"]').bind('input',calcAndShow);
+
+function calcAndShow(){
+    var hours = parseFloat($('input[name="calc-hour-day"]:checked').val());
+    var day = parseFloat($('input[name="calc-day-week"]:checked').val());
+    var earn = parseFloat($('input[name="calc-earn-for"]:checked').val());
+
+    $('#summ').html('$'+8*hours*day*earn)
+}
