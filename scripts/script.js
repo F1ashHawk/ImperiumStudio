d05974c647a9140e16c83221d6c49fd522700cfd
F1ashHawk <1nik2121@gmail.com>
2022-07-03 20:00:20 +0300

    complited

--- scripts/script.js
+++ scripts/script.js
@@ -1,97 +1,55 @@
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// /* Функция увеличивает индекс на 1, показывает следующй слайд*/
-// function plusSlide() {
-//     showSlides(slideIndex += 1);
-// }
-
-// /* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
-// function minusSlide() {
-//     showSlides(slideIndex -= 1);
-// }
-
-// /* Устанавливает текущий слайд */
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
-
-// /* Основная функция слайдера */
-// function showSlides(n) {
-//     var i;
-//     var slides = document.getElementsByClassName("about-slider__item");
-//     var dots = document.getElementsByClassName("slider-dots_item");
-//     if (n > slides.length) {
-//         slideIndex = 1
-//     }
-//     if (n < 1) {
-//         slideIndex = slides.length
-//     }
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex - 1].style.display = "block";
-//     // dots[slideIndex - 1].className += " active";
-// }
-
 $('.about-slider-list').slick({
-    infinite: true,
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    variableWidth: true,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                dots: false
-            }
-        }
-    ]
+  infinite: true,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        dots: false
+      }
+    }
+  ]
 })
 
-  $('.responsive').slick({
-    infinite: true,
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          variableWidth: true
-        }
+$('.responsive').slick({
+  infinite: true,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true
+      }
+    }
+  ]
+});
 // function() {
 //     var $videoContainer = $('#video'),
 //         $videoControls = $('.video-control'),
@@ -110,71 +68,101 @@ $('.about-slider-list').slick({
 //     });
 // };
 
-// $('.multiple-items').slick({
-//     infinite: true,
-//     dots: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 1
-// });
 
-document.getElementById('start-form-open').addEventListener('click',function(){
-    document.getElementById('start-form-overlay').classList.remove('start-form-overlay-close')
+document.getElementById('start-form-open').addEventListener('click', function () {
+  document.getElementById('start-form-overlay').classList.remove('start-form-overlay-close')
 })
 
-document.getElementById('close-start-form-button').addEventListener('click',function(){
-    document.getElementById('start-form-overlay').classList.add('start-form-overlay-close')
+document.getElementById('close-start-form-button').addEventListener('click', function () {
+  document.getElementById('start-form-overlay').classList.add('start-form-overlay-close')
 })
 
-
-
-// document.getElementById('start-form-overlay').addEventListener('click',function(){
-//     document.getElementById('start-form-overlay').classList.add('start-form-overlay-close')
-// })
-
-document.getElementById('header-burger').addEventListener('click', function(){
-    document.getElementById('burger-form-background').classList.remove('burger-form-background-close')
+document.getElementById('header-burger').addEventListener('click', function () {
+  document.getElementById('burger-form-background').classList.remove('burger-form-background-close')
 })
 
-document.getElementById('close-burger').addEventListener('click', function(){
-    document.getElementById('burger-form-background').classList.add('burger-form-background-close')
+document.getElementById('close-burger').addEventListener('click', function () {
+  document.getElementById('burger-form-background').classList.add('burger-form-background-close')
 })
 
 
 
-$('input[name="calc-hour-day"],input[name="calc-day-week"],input[name="calc-earn-for"]').bind('input',calcAndShow);
+$('input[name="calc-hour-day"],input[name="calc-day-week"],input[name="calc-earn-for"]').bind('input', calcAndShow);
 
-function calcAndShow(){
-    var hours = parseFloat($('input[name="calc-hour-day"]:checked').val());
-    var day = parseFloat($('input[name="calc-day-week"]:checked').val());
-    var earn = parseFloat($('input[name="calc-earn-for"]:checked').val());
+function calcAndShow() {
+  var hours = parseFloat($('input[name="calc-hour-day"]:checked').val());
+  var day = parseFloat($('input[name="calc-day-week"]:checked').val());
+  var earn = parseFloat($('input[name="calc-earn-for"]:checked').val());
 
-    $('#summ').html('$'+8*hours*day*earn)
+  $('#summ').html('$' + 8 * hours * day * earn)
 }
-// const animItems = document.querySelectorAll('.anim-items');
 
-// if (animItems.length > 0) {
-//   function animOnScroll(params) {
-//     for (let i = 0; i < animItems.length; i++){
-//       const animItem = animItems[i];
-//       const animItemHeight = animItem.offsetHeight;
-//       const animItemOffset = offset (animItem).top;
-//       const animStart = 4;
-
-//       let animItemPoint = window.innerHeight - animItemHeight / animStart;
-
-//       if(animItemHeight > window.innerHeight) {
-//         animItemPoint = window.innerHeight - animItemHeight / animStart;
-//       }
-
-//       if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight))
-//     }
-//   }
+const animItems = document.querySelectorAll('.anim-items');
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll(params) {
+    for (let i = 0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
 
 
-//   function offset(el) {
-//     const rect = el.getBoundingClientRect(),
-//       scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-//       scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//     return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
-//   }
-// }
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - animItemHeight / animStart;
+      }
+
+      if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('anim-active');
+      } else {
+        animItem.classList.remove('anim-active');
+      }
+    }
+  }
+
+
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+}
+
+var sections = $('section'),
+nav = $('.page-nav-ul'),
+burger = $('.burger-nav-menu-ul')
+nav_height = nav.outerHeight();
+
+$(window).on('scroll', function(){
+  var cur_pos = $(this).scrollTop();
+
+  sections.each(function(){
+    var top =$(this).offset().top - nav_height,
+    bottom = top +$(this).outerHeight();
+
+    if(cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('page-nav-link-active');
+      burger.find('a').removeClass('burger-nav-menu-a-active');
+      sections.removeClass('active');
+
+      $(this).addClass('active');
+
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('page-nav-link-active');
+      burger.find('a[href="#'+$(this).attr('id')+'"]').addClass('burger-nav-menu-a-active');
+    }
+  })
+});
+
+nav.find('a').on('click', function(){
+  var $el = $(this),
+  id = $el.attr('href');
+
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+
+  return false;
+})
